9447e3deb6e1009cd8647a92990a5dc26cc72710
unknown <you@example.com>
2019-07-17 15:16:15 +0530

    add method added

--- Hungery.java
+++ Hungery.java
@@ -1,5 +1,12 @@
 public class Hungery{
+	public static void add(int a , int b){
+		int c=0;
+		c=a+b;
+		System.out.println("Sum Of A And B Is :: "+c);
+
+	}
 	public static void main(String [] args){
 		System.out.println("I am hungery...!!!");
+		add(10,20);
 	}
 }
